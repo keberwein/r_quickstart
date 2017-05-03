57df2961127362d574d6d69d78b17bd9cd3caa9e
Keberwein <kristian.eberwein@yahoo.com>
2017-05-03 15:41:23 -0400

    ported for CentOS 7, added newest versions of Shiny and Server

--- r_quickstart.sh
+++ r_quickstart.sh
@@ -37,8 +37,8 @@ sudo apt-get -yy install openjdk-7-jdk
 export LD_LIBRARY_PATH=/usr/lib/jvm/java-7-openjdk-amd64/jre/lib/amd64/server
 sudo R CMD javareconf  
 sudo su - -c "R -e \"install.packages(c('shiny', 'rmarkdown', 'devtools'), repos='http://cran.rstudio.com/')\""
-sudo wget https://download3.rstudio.org/ubuntu-12.04/x86_64/shiny-server-1.5.1.834-amd64.deb
-sudo gdebi shiny-server-1.5.1.834-amd64.deb
+wget https://download3.rstudio.org/ubuntu-12.04/x86_64/shiny-server-1.5.3.838-amd64.deb
+sudo gdebi shiny-server-1.5.3.838-amd64.deb
 echo ""
 echo ""
 echo "Setting permissions for Shiny"
--- r_quickstart_cent7.sh
+++ r_quickstart_cent7.sh
@@ -1,12 +1,12 @@
-# R quick upstart script for CentOS 6
+# R quick upstart script for CentOS 7
 
 sudo yum update
 
 sudo yum install wget -y
 
 # Get the EPEL rpm
-wget http://mirror.us.leaseweb.net/epel/6/i386/epel-release-6-8.noarch.rpm
-rpm -ivh epel-release-6-8.noarch.rpm
+sudo yum install epel-release
+sudo yum update
 
 # Import the EPEL GPG key
 wget https://www.fedoraproject.org/static/0608B895.txt
@@ -34,9 +34,6 @@ sudo su - -c "R -e \"install.packages(c('shiny', 'rmarkdown', 'devtools', 'RJDBC
 wget https://download3.rstudio.org/centos5.9/x86_64/shiny-server-1.5.3.838-rh5-x86_64.rpm
 sudo yum install --nogpgcheck shiny-server-1.5.3.838-rh5-x86_64.rpm -y
 
-# Install Upstart script
-sudo wget https://raw.github.com/rstudio/shiny-server/master/config/upstart/shiny-server.conf -O /etc/init/shiny-server.conf
-
 # Create a root directory for shiny website
 sudo mkdir -p /var/shiny-server/www
 
@@ -47,8 +44,8 @@ sudo mkdir -p /var/shiny-server/log
 sudo start shiny-server
 
 # Edit firewall rules CentOS 6
-sudo iptables -I INPUT -p tcp -m tcp --dport 3838 -j ACCEPT
-sudo service iptables save
+sudo firewall-cmd --zone=public --add-port=3838/tcp --permanent
+firewall-cmd --reload
 
 ################# Rstudio Server setup
 
@@ -56,11 +53,5 @@ wget https://download2.rstudio.org/rstudio-server-rhel-1.0.143-x86_64.rpm
 sudo yum install --nogpgcheck rstudio-server-rhel-1.0.143-x86_64.rpm -y
 
 # Edit firewall rules CentOS 6
-sudo iptables -I INPUT -p tcp -m tcp --dport 8787 -j ACCEPT
-sudo service iptables save
-
-# Add group and users for RStudio Server
-
-groupadd rstudio_users
-usermod -a -G rstudio_users keberwein
-chown -R keberwein:rstudio_users /home/keberwein
+sudo firewall-cmd --zone=public --add-port=8787/tcp --permanent
+firewall-cmd --reload
